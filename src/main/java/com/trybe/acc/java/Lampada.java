42559f2e24133ad6f9347f6511ef600bf37ef7c3
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-03-20 14:14:57 -0300

    Initial commit

---
+++ src/main/java/com/trybe/acc/java/Lampada.java
@@ -0,0 +1,13 @@
+package com.trybe.acc.java;
+
+public class Lampada {
+
+	public static void main(String[] args) {
+			// Seu código aqui
+						
+			// Imprime a saída no console. OBS: descomente a próxima linha
+			//System.out.println("A lampada ligada: " + isLigada + "!");
+
+	}
+
+}
